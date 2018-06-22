a9f2c067936245bfc07410ee4615d39b03ce7c79
Rosco Kalis <roscokalis@gmail.com>
2018-06-22 13:14:47 +0200

    Removed Adoption and added Roulette

---
+++ contracts/Roulette.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.22;
+
+contract Roulette {
+    uint256 public maxBetDivisor;
+    address public owner;
+
+    event PlayEvent(address player, uint256 betSize, uint betNumber, uint winningNumber);
+
+    constructor(uint256 _maxBetDivisor) public {
+        owner = msg.sender;
+        if (_maxBetDivisor == 0) {
+            maxBetDivisor = 100;
+        } else {
+            maxBetDivisor = _maxBetDivisor;
+        }
+    }
+
+    function kill() public {
+        require(msg.sender == owner, "Only the owner can kill this contract");
+        selfdestruct(owner);
+    }
+
+    function fund() public payable {}
+    function() public payable {}
+
+    function bet(uint number) public payable {
+        require(msg.value <= getMaxBet(), "Bet amount can not exceed max bet size");
+        require(msg.value > 0, "A bet should be placed");
+
+        uint winningNumber = generateWinningNumber();
+        emit PlayEvent(msg.sender, msg.value, number, winningNumber);
+        if (number == winningNumber) {
+            payout(msg.sender, msg.value * 36);
+        }
+    }
+
+    function getMaxBet() public view returns (uint256) {
+        return address(this).balance / maxBetDivisor;
+    }
+
+    function generateWinningNumber() public view returns (uint) {
+        return block.number % 37;
+    }
+
+    function payout(address winner, uint256 amount) internal {
+        require(amount > 0);
+        require(amount <= address(this).balance);
+        winner.transfer(amount);
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Adoption = artifacts.require("Adoption");
+var Roulette = artifacts.require("Roulette");
 
 module.exports = function(deployer) {
-  deployer.deploy(Adoption);
+    deployer.deploy(Roulette, 100);
 };
