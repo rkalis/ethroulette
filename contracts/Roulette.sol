a3b29b9d64aab6b7132207dfa2dc6d534069a580
Rosco Kalis <roscokalis@gmail.com>
2018-07-28 19:28:39 +0200

    Added investment  capabilities
    - Added token <-> ether conversion functions
    - Partially implemented token as ERC20
    - Added invest and divest functions that buy/sell tokens
    - Added basic tests for these functions

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.4.23;
 
-import "oraclize-api/usingOraclize.sol";
+import "oraclize-api/contracts/usingOraclize.sol";
+import "zeppelin/contracts/token/ERC20Basic.sol";
 
-contract Roulette is usingOraclize {
-    uint256 public token;
+// TODO: Safe math
+
+contract Roulette is usingOraclize, ERC20Basic {
+    uint256 public tokenPrice;
+    uint256 public totalSupply;
     address public owner;
 
     enum CashflowType {
@@ -17,13 +21,15 @@ contract Roulette is usingOraclize {
         uint8 betNumber;
     }
 
-    // mapping(address=>uint256) investorBalances;
+    mapping(address=>uint256) investorBalances;
     mapping(bytes32=>PlayerInfo) players;
 
-    event LogBet(address player, uint256 betSize, uint8 betNumber);
-    event LogPlay(address player, uint256 betSize, uint8 betNumber, uint8 winningNumber);
-    event LogPayout(address winner, uint256 payout);
-    // event LogInvest(address investor, uint256 ethAmount, uint256 tokenPrice, uint256 tokenAmount);
+    event Bet(address indexed player, uint256 betSize, uint8 betNumber);
+    event Play(address indexed player, uint256 betSize, uint8 betNumber, uint8 winningNumber);
+    event Payout(address indexed winner, uint256 payout);
+    event Invest(address indexed investor, uint256 ethAmount, uint256 tokenPrice, uint256 tokenAmount);
+    event Divest(address indexed investor, uint256 ethAmount, uint256 tokenPrice, uint256 tokenAmount);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event LogDebugInteger(uint256 integer);
 
@@ -32,7 +38,7 @@ contract Roulette is usingOraclize {
 
         /* Set OAR for use with ethereum-bridge, remove for productio  */
         OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
-        token = 1 ether;
+        tokenPrice = 1 ether;
     }
 
     function kill() external {
@@ -40,16 +46,67 @@ contract Roulette is usingOraclize {
         selfdestruct(owner);
     }
 
-    function fund() external payable {}
     function() external payable {}
+
+    // ERC20Basic functions
+
+    function balanceOf(address who) public view returns (uint256) {
+        return investorBalances[who];
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(value <= investorBalances[msg.sender]);
+        require(to != address(0));
+
+        investorBalances[msg.sender] -= value;
+        investorBalances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    // ///////////////////////
+
+    // Investment functions
+
+    function invest() external payable {
+        require(msg.value > 0, "An investment should be made");
+
+        uint256 tokenAmount = convertEthToToken(msg.value);
+        investorBalances[msg.sender] += tokenAmount;
+
+        emit Invest(msg.sender, msg.value, tokenPrice, tokenAmount);
+    }
+
+    function divest(uint256 tokenAmount) external {
+        require(tokenAmount <= investorBalances[msg.sender], "Can not divest more than investment");
+
+        investorBalances[msg.sender] -= tokenAmount;
+
+        uint256 ethAmount = convertTokenToEth(tokenAmount);
+        msg.sender.transfer(ethAmount);
+
+        emit Divest(msg.sender, ethAmount, tokenPrice, tokenAmount);
+    }
+
+    function convertEthToToken(uint256 ethAmount) public view returns (uint256) {
+        return (ethAmount * 1 ether) / tokenPrice;
+    }
+
+    function convertTokenToEth(uint256 tokenAmount) public view returns (uint256) {
+        return (tokenPrice * tokenAmount) / 1 ether;
+    }
+
+    // ///////////////////////
+
+    // Betting functions
 
     function bet(uint8 number) external payable {
         require(msg.value <= getMaxBet(), "Bet amount can not exceed max bet size");
         require(msg.value > 0, "A bet should be placed");
 
-        updateTokenValue(address(this).balance - msg.value, msg.value, CashflowType.Inflow);
+        updateTokenPrice(address(this).balance - msg.value, msg.value, CashflowType.Inflow);
 
-        emit LogBet(msg.sender, msg.value, number);
+        emit Bet(msg.sender, msg.value, number);
         bytes32 qid = oraclize_query("WolframAlpha", "random integer between 0 and 1");
 
         /* Store a player's info to retrieve it in the oraclize callback */
@@ -63,7 +120,7 @@ contract Roulette is usingOraclize {
         uint8 winningNumber = uint8(parseInt(result));
         PlayerInfo storage playerInfo = players[qid];
 
-        emit LogPlay(playerInfo.player, playerInfo.betSize, playerInfo.betNumber, winningNumber);
+        emit Play(playerInfo.player, playerInfo.betSize, playerInfo.betNumber, winningNumber);
 
         if (playerInfo.betNumber == winningNumber) {
             payout(playerInfo.player, playerInfo.betSize * 36);
@@ -81,13 +138,15 @@ contract Roulette is usingOraclize {
         require(amount > 0);
         require(amount <= address(this).balance);
 
-        updateTokenValue(address(this).balance, amount, CashflowType.Outflow);
+        updateTokenPrice(address(this).balance, amount, CashflowType.Outflow);
 
         winner.transfer(amount);
-        emit LogPayout(winner, amount);
+        emit Payout(winner, amount);
     }
 
-    function updateTokenValue(uint256 _balance, uint256 _change, CashflowType _cashflowType) internal returns (uint256) {
+    // ///////////////////////
+
+    function updateTokenPrice(uint256 _balance, uint256 _change, CashflowType _cashflowType) internal returns (uint256) {
         emit LogDebugInteger(_balance);
         emit LogDebugInteger(_change);
 
@@ -100,6 +159,6 @@ contract Roulette is usingOraclize {
         }
 
         emit LogDebugInteger(multiplier);
-        token = token * multiplier / 1 ether;
+        tokenPrice = tokenPrice * multiplier / 1 ether;
     }
 }
--- test/roulette.test.js
+++ test/roulette.test.js
@@ -7,39 +7,95 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
+// TODO: Add more edge case tests
+
 contract('roulette', async (accounts) => {
     let roulette;
 
-    const fundingAccount = accounts[0];
+    const ownerAccount = accounts[0];
     const bettingAccount = accounts[1];
-    const fundingSize = 100;
+    const investmentSize = 1000000;
+
+    let debugEvent;
 
     beforeEach(async () => {
-        roulette = await Roulette.new({from: fundingAccount});
-        await roulette.fund({from: fundingAccount, value: fundingSize});
-        assert.equal(web3.eth.getBalance(roulette.address).toNumber(), fundingSize);
+        roulette = await Roulette.new({from: ownerAccount});
+
+        debugEvent = roulette.LogDebugInteger({ fromBlock: 'latest' });
+        debugEvent.watch((error, log) => {
+            console.log(log.args.integer.toNumber())
+        });
     });
     afterEach(async () => {
-        await roulette.kill({from: fundingAccount});
+        debugEvent.stopWatching();
+        await roulette.kill({from: ownerAccount});
     });
 
     it("should play", async () => {
-        roulette.LogDebugInteger({ fromBlock: 'latest' }).watch((error, log) => {
-            console.log(log.args.integer.toNumber())
-        });
+        // await roulette.invest({from: ownerAccount, value: investmentSize});
 
-        console.log(web3.eth.getBalance(roulette.address).toNumber());
-        console.log((await roulette.token()).toNumber());
-        console.log('---------------------------------');
-        let res = await roulette.bet(1, {from: bettingAccount, value: 1});
-        let log = await promisifyLogWatch(roulette.LogPlay({ fromBlock: 'latest' }));
-        console.log('---------------------------------');
-        console.log(log.args.betNumber.toNumber());
-        console.log(log.args.winningNumber.toNumber());
-        console.log('---------------------------------');
-        console.log(web3.eth.getBalance(roulette.address).toNumber());
-        console.log((await roulette.token()).toNumber());
+        // console.log(web3.eth.getBalance(roulette.address).toNumber());
+        // console.log((await roulette.tokenPrice()).toNumber());
+        // console.log('---------------------------------');
+        // let res = await roulette.bet(1, {from: bettingAccount, value: 1});
+        // let log = await promisifyLogWatch(roulette.LogPlay({ fromBlock: 'latest' }));
+        // console.log('---------------------------------');
+        // console.log(log.args.betNumber.toNumber());
+        // console.log(log.args.winningNumber.toNumber());
+        // console.log('---------------------------------');
+        // console.log(web3.eth.getBalance(roulette.address).toNumber());
+        // console.log((await roulette.tokenPrice()).toNumber());
     })
+
+    it("can invest", async () => {
+        // given
+        let tokenPriceBeforeInvestment = (await roulette.tokenPrice()).toNumber();
+
+        // when
+        let tx = await roulette.invest({from: ownerAccount, value: investmentSize});
+
+        // then
+        assert.equal(web3.eth.getBalance(roulette.address).toNumber(), investmentSize, "Contract balance should equal investment size");
+
+        let expectedTokenBalance = investmentSize / tokenPriceBeforeInvestment * 1e18;
+        assert.equal((await roulette.balanceOf(ownerAccount)).toNumber(), expectedTokenBalance, "Token balance should match");
+
+        assert.equal(tokenPriceBeforeInvestment, (await roulette.tokenPrice()).toNumber(), "Token price should not change");
+
+        truffleAssert.eventEmitted(tx, 'Invest', (ev) => {
+            return ev.investor == ownerAccount && ev.ethAmount == investmentSize &&
+                   ev.tokenPrice == tokenPriceBeforeInvestment && ev.tokenAmount == expectedTokenBalance;
+        })
+    })
+
+    it("can divest", async () => {
+        // given
+        await roulette.invest({from: ownerAccount, value: investmentSize});
+
+        let tokenPriceBeforeDivestment = (await roulette.tokenPrice()).toNumber();
+
+        let divestmentSizeInEth = investmentSize / 2;
+        let divestmentSizeInTokens = divestmentSizeInEth / tokenPriceBeforeDivestment * 1e18;
+        let remainingInvestment = investmentSize - divestmentSizeInEth;
+
+        // when
+        let tx = await roulette.divest(divestmentSizeInTokens, {from: ownerAccount});
+
+        // then
+        assert.equal(web3.eth.getBalance(roulette.address).toNumber(), remainingInvestment, "Contract balance should equal the remaining investment");
+
+        let expectedTokenBalance = remainingInvestment / tokenPriceBeforeDivestment * 1e18;
+        assert.equal((await roulette.balanceOf(ownerAccount)).toNumber(), expectedTokenBalance, "Token balance should match");
+
+        assert.equal(tokenPriceBeforeDivestment, (await roulette.tokenPrice()).toNumber(), "Token price should not change");
+
+        truffleAssert.eventEmitted(tx, 'Divest', (ev) => {
+            return ev.investor == ownerAccount && ev.ethAmount == divestmentSizeInEth &&
+                   ev.tokenPrice == tokenPriceBeforeDivestment && ev.tokenAmount == divestmentSizeInTokens;
+        });
+    })
+
+
 
     // it("should lose when bet on the wrong number", async () => {
     //     // given
