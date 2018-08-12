bf3aea46ecc930145a7e290f41eaf6a6bcb738df
Rosco Kalis <roscokalis@gmail.com>
2018-08-12 16:32:10 +0200

    Added Rinkeby deployment profile

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,6 +10,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 8000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/${process.env.INFURA_API_KEY}`)
+      },
+      network_id: 4,
       gasPrice: 10000000000
     }
   }
