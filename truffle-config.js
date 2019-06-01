6772037973b686e7e31a50ef61cb7351fb17be57
Rosco Kalis <roscokalis@gmail.com>
2019-06-02 02:52:50 +0900

    Add truffle-plugin-verify

--- truffle-config.js
+++ truffle-config.js
@@ -15,8 +15,13 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
       },
-      network_id: 4,
-      gasPrice: 10e9
+      network_id: 4
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
   }
 };
